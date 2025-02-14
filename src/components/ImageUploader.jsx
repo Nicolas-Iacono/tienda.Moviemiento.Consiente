649375969195d59cd91c36fa48a3c16878812edd
Nicolas Iacono <nicoiacono96@gmail.com>
2025-02-14 02:57:54 -0300

    problemas con supabaseUrl2

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -6,7 +6,7 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 // Inicializa el cliente de Supabase
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://jogykgijckiappwwefld.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvZ3lrZ2lqY2tpYXBwd3dlZmxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzNzQ4MTgsImV4cCI6MjA0OTk1MDgxOH0.X-DdqQ8_jZuLy0mbc2wTHD9KrNLWM3GutQFxnE2QKqU";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const ImageUploader = ({ onUploadComplete }) => {
--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://jogykgijckiappwwefld.supabase.co"
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvZ3lrZ2lqY2tpYXBwd3dlZmxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzNzQ4MTgsImV4cCI6MjA0OTk1MDgxOH0.X-DdqQ8_jZuLy0mbc2wTHD9KrNLWM3GutQFxnE2QKqU"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
